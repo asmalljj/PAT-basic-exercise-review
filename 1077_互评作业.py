fc3a08da25be5e5171f79588a1a36dab3d6e74a8
asmalljj <653595478@qq.com>
2025-09-19 14:20:38 +0800

    版本2：文件读取

--- 1077_互评作业.py
+++ 1077_互评作业.py
@@ -1,17 +1,29 @@
-N, M = map(int,input().split())
 
-for i in range(N):
-    score = list(map(int, input().split()))
-    teacher_Score = score[0]
-    others_score = score[1:]
 
-    valid_Scores = []
-    for _ in others_score:
-        if 0 <= _ <= M:
-            valid_Scores.append(_)
 
-    max_others_score = max(valid_Scores)
-    min_others_score = min(valid_Scores)
-    g1 = (sum(valid_Scores) - max_others_score - min_others_score) / (len(valid_Scores) - 2)
-    avg_final = int(((g1 + teacher_Score) / 2) + 0.5)
-    print(avg_final)
+def main():
+    # 从文件读取数据
+    with open('input.txt', 'r') as f:
+        data = f.readline().split()
+        N, M = int(data[0]), int(data[1])
+
+        for i in range(N):
+            score = list(map(int, f.readline().split()))
+            teacher_Score = score[0]
+            others_score = score[1:]
+
+            valid_Scores = [s for s in others_score if 0 <= s <= M]
+
+            if len(valid_Scores) < 2:
+                avg_final = int(teacher_Score + 0.5)
+            else:
+                max_score = max(valid_Scores)
+                min_score = min(valid_Scores)
+                g1 = (sum(valid_Scores) - max_score - min_score) / (len(valid_Scores) - 2)
+                avg_final = int(((g1 + teacher_Score) / 2) + 0.5)
+
+            print(avg_final)
+
+
+if __name__ == "__main__":
+    main()
