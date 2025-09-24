4fb0553de088c85d533d888e1e56a9e86ee0fffb
asmalljj <653595478@qq.com>
2025-09-24 16:12:42 +0800

    列表和切片的使用分割，四舍五入的基本算法，map()的应用

--- 1077_互评作业.py
+++ 1077_互评作业.py
@@ -15,3 +15,4 @@ for i in range(N):
     g1 = (sum(valid_Scores) - max_others_score - min_others_score) / (len(valid_Scores) - 2)
     avg_final = int(((g1 + teacher_Score) / 2) + 0.5)
     print(avg_final)
+'''最终版本吧 反正原因就是input最后一组需要回车才会说输入 也许只有放在文件里读取才不会出问题'''
